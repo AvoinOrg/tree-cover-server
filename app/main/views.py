a3d328f012f8c02bbc69cdfda8ea60c298e5a94a
lmorganti19 <56442457+lmorganti19@users.noreply.github.com>
2019-11-12 16:35:05 +0200

    Update views.py

--- app/main/views.py
+++ app/main/views.py
@@ -7,6 +7,7 @@ from .forms import FileForm
 from .process_data import process_file
 from werkzeug.utils import secure_filename
 
+from flask import send_from_directory
 
 @main.route("/", methods=["GET", "POST"])  # or e.g. /upload if want other home page
 def index():
@@ -18,10 +19,33 @@ def index():
             session["file_name"] = secure_filename(f.filename)
             f_path = os.path.join("files", session["file_name"])
             f.save(f_path)  # todo: current_app.instance_path, ... instead
+            
+            #creates a processed file in "files"
+            d = process_file(f_path)
+            dpath = os.path.join("files","results_" +session["file_name"])
+            d.to_csv(dpath)
+            
             flash("You have uploaded a file!")
-            return render_template("result_list.html", file_name=session.get("file_name"), data=process_file(f_path))
+            return render_template("result_list.html", file_name=session.get("file_name"), data=d)
         except Exception as e:
             flash("Error during upload")
             raise e
             # return redirect(url_for('.index'))
     return render_template("index.html", form=form, file_name=session.get("file_name"))  # upload.html
+
+
+#download function retrieves processed file from "files" and downloads it in Downloads
+@main.route("/get-csv/<csv_id>")
+def get_csv(csv_id):
+    csv_name = "results_"+csv_id
+    csv = os.path.abspath("files")
+
+    print(csv_name)
+    try:
+        
+        return send_from_directory(csv, filename=csv_name, as_attachment=True)
+    
+    except Exception as e:
+            flash("Error during download")
+            raise e
+    
